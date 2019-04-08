19a6555e8d5dc72fb54705cd278628f2646ccc1f
Mahmoud.Zakria <mahmoud.zakria@dunnhumby.com>
2019-04-08 16:07:28 +0200

    share button test

--- webhooks.js
+++ webhooks.js
@@ -58,6 +58,11 @@ module.exports = {
                 title: "AskZak",
                 image_url: "https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/55840604_802209380163090_4085643487779749888_n.jpg?_nc_cat=109&_nc_ht=scontent-amt2-1.xx&oh=6e3e1d0f2773689142e452498d26e267&oe=5D49D702",
                 subtitle: "Ask me anything related to the web industry",
+                default_action: {
+                  type: "web_url",
+                  url: "https://www.facebook.com/AskZak-802208680163160/?modal=admin_todo_tour",
+                  webview_height_ratio: "tall"
+                },
                 buttons: [
                   {
                     type: "element_share"
